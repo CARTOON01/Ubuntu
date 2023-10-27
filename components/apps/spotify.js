2fbf460af95f989ac0f30cc93c08cb7517ca53ac
CARTOON01 <brianngugi224@gmail.com>
2023-10-27 17:39:16 +0300

    Ubuntu

---
+++ components/apps/spotify.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Spotify() {
+    return (
+        <iframe src="https://open.spotify.com/playlist/35QZco3h3UELAC2VsLmpSD?si=855215e0190c484e" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
+    )
+}
+
+export const displaySpotify = () => {
+    <Spotify> </Spotify>
+}
