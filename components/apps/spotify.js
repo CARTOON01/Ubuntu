cbe25894e339e5f724d2229656c7c42e136e01d4
CARTOON01 <brianngugi224@gmail.com>
2023-10-29 05:09:09 +0300

    Spotify

--- components/apps/spotify.js
+++ components/apps/spotify.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Spotify() {
     return (
-        <iframe src="https://open.spotify.com/playlist/35QZco3h3UELAC2VsLmpSD?si=855215e0190c484e" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/35QZco3h3UELAC2VsLmpSD?si=855215e0190c484e" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
     )
 }
 
